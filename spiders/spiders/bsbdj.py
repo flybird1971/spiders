8196e2dd636ac2eeac93ad694108879e8ff5541d
flybird1971 <flybird1971@gmail.com>
2016-03-18 19:19:10 +0800

    mysql pdo

--- spiders/spiders/bsbdj.py
+++ spiders/spiders/bsbdj.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-#coding:utf-8
+# coding:utf-8
 
 from scrapy.spiders import Spider
 from scrapy.http import Request
 from spiders.items import BsbdjItem
 import re
+
 
 class BsbdjSpider(Spider):
 
@@ -23,16 +24,17 @@ class BsbdjSpider(Spider):
         'http://www.budejie.com/new-audio/',
     ]
 
-    def parse(self,response):
+    def parse(self, response):
 
         sels = response.xpath('/html/body/div[2]/div/div[1]/div[1]/div[2]/ul/li')
         for sel in sels:
             item = BsbdjItem()
-            item['name'] = [ n.encode('utf-8') for n in sel.xpath('div[2]/div[1]/text()').extract() ]
-            item['url'] = [ (self.name + url).encode('utf-8') for url in sel.xpath('div[3]/div[3]/ul/li[3]/a/@href').extract()]
+            item['name'] = [n.encode('utf-8') for n in sel.xpath('div[2]/div[1]/text()').extract()]
+            item['url'] = [(self.name + url).encode('utf-8')
+                           for url in sel.xpath('div[3]/div[3]/ul/li[3]/a/@href').extract()]
             yield item
 
         newUrl = response.xpath('/html/body/div[2]/div/div[1]/div[2]/div/a[@class="z-crt"]/@href').extract()
         pageNum = int(newUrl[0]) + 1
-        newUrl = self.regex.sub(r'\1',response.url) + str(pageNum)
-        yield Request(newUrl,callback=self.parse)
+        newUrl = self.regex.sub(r'\1', response.url) + str(pageNum)
+        yield Request(newUrl, callback=self.parse)
